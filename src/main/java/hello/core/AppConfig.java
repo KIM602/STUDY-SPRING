b51998c6f84e89ddfb28ee168ded795b2e9d329c
KIM602 <kyk940608@gmail.com>
2023-03-28 20:58:28 +0900

    스프링으로 전환하기 COMMIT

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,31 +8,40 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     //중복제거 및 역할에 따라 분리(리팩터링)
     //역할과 구조가 한눈에 들어옴, 애플리케이션 전체구성 빠르게 파악 가능
 
+    @Bean
     //MemberService 역할
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository()); //생성자 주입
     }
 
+    @Bean
     //MemberRepository 역할
-    private static MemberRepository memberRepository() {
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     //OrderService 역할
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     //DiscountPolicy 역할
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
 
+    //스프링의 장점
+    //스프링 컨테이너가 관리를 해줌으로써 편리한 기능이 매우 많음
 }
